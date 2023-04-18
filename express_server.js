5499ff80db1f1e56a1c385ef4e6fb88764fc1ca7
Arian Ahmadi <arianahmadi1@gmail.com>
2023-04-18 06:38:10 -0400

    url_index updated

--- express_server.js
+++ express_server.js
@@ -7,10 +7,15 @@ app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-};
+};  
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+
+  res.render("urls_index", { urlDatabase } );
 });
 
 app.get("/hello", (req, res) => {
