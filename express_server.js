28c4296f54adcccb92833043bf7bab28f5a998a4
Arian Ahmadi <arianahmadi1@gmail.com>
2023-04-19 07:31:10 -0400

    express_server.js modified

--- express_server.js
+++ express_server.js
@@ -10,26 +10,41 @@ function generateRandomString() {
   }
   return result;
 }
-
-
-app.set("view engine", "ejs");
-
-app.use(express.urlencoded({ extended: true }));
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };  
 
+app.set("view engine", "ejs"); //set EJS as our view engine
 
+
+// Add body-parser middleware to parse the POST request body
+app.use(express.urlencoded({ extended: true }));
+
+
+// app.post("/urls", (req, res) => {
+//   console.log(req.body); // Log the POST request body to the console
+//   res.send("Ok"); // Respond with 'Ok' (we will replace this)
+// });
+
+// POST route to receive form submission and save longURL and shortURL to urlDatabase
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+
+//redirect to longURL when short URL is used. @ victor 
+app.get("/u/:id", (req, res) => {
+  const longURL= urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
 app.get("/urls", (req, res) => {
 
@@ -47,9 +62,9 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
